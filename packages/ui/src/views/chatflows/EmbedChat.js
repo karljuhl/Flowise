b2356ccf9006f9f1fe31b76389deb0814381f2a8
Karl Juhl <53413191+karljuhl@users.noreply.github.com>
2023-08-15 18:30:48 +0200

    Update EmbedChat.js

--- packages/ui/src/views/chatflows/EmbedChat.js
+++ packages/ui/src/views/chatflows/EmbedChat.js
@@ -195,7 +195,7 @@ const App = () => {
 const embedFullpageHtmlCodeCustomization = (chatflowid) => {
     return `<flowise-fullchatbot></flowise-fullchatbot>
 <script type="module">
-    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    import Chatbot from "https://cdn.jsdelivr.net/gh/karljuhl/FlowiseChatEmbed/dist/web.js"
     Chatbot.initFull({
         chatflowid: "${chatflowid}",
         apiHost: "${baseURL}",
